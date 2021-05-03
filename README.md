0016696bd5ab6d47736c1be1b77c54ca63d5bc04
ftrtz <fabian.treitz@gmail.com>
2021-05-03 09:00:17 +0200

    added docker compose and volume binding

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # ai_application
 
-RAYLYTIC CodingChallenge - see main.ipynb for Documentation
+RAYLYTIC CodingChallenge - see [documentation.ipynb](./documentation.ipynb)
+
+## Run main.py with docker to generate and store results
+
+Build docker image
+
+```shell
+$ docker build --tag ai_application .
+```
+
+Start docker container
+
+```shell
+$ docker-compose up
+```
+
+Results are stored in [/results](./results)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import datetime
+import os.path
+import pandas as pd
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
 from tensorflow.keras.datasets.mnist import load_data
 from ai_library import CustomData, NeuralNet, Visualisation
@@ -12,7 +14,7 @@ class MNISTdata(CustomData):
         return load_data()
 
 # parameters
-EPOCHS = 1
+EPOCHS = 30
 BATCH_SIZE = 128
 
 # load MNIST data
@@ -33,7 +35,8 @@ model = NeuralNet(num_classes)
 # early stopping
 early_stopping = EarlyStopping(monitor="val_loss", mode="min", patience=2)
 # tensorboard
-log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+logtime = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_dir = "results/logs/fit/" + logtime
 tensorboard_cb = TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 callbacks = [early_stopping, tensorboard_cb]
@@ -41,22 +44,39 @@ callbacks = [early_stopping, tensorboard_cb]
 # train model
 history = model.comp_and_fit(x_train, y_train, EPOCHS, BATCH_SIZE, callbacks)
 
+# save trained model
+model.save("results/model")
+
 # model evaluation
 loss, accuracy = model.test_eval(x_test, y_test)
 
 # predictions
 y_preds = model.test_pred(x_test)
 
-# Figures
+
+# SAVE RESULTS
+# save loss and accuracy in csv
+d = {"logtime": logtime, "loss": loss, "accuracy": accuracy}
+eval_df = pd.DataFrame(data=d, index=[0])
+
+if os.path.isfile("results/loss_acc.csv"):
+    header = False
+else:
+    header = True
+
+eval_df.to_csv("results/loss_acc.csv", mode="a", index=False, index_label=False, header=header)
+
+# figures
 viz = Visualisation()
 # confusion matrix
 cm = viz.cm_plot(y_test, y_preds, labels_test)
 # image samples
-sample_act = viz.img_plot("Sample Images and corresponding labels", x_train, y_train)
-sample_pred = viz.img_plot("Sample images and predicted labels", x_test, y_preds)
+sample_img = viz.img_plot("Sample Images and corresponding labels", x_train, y_train)
+sample_img_pred = viz.img_plot("Sample images and predicted labels", x_test, y_preds)
 
-cm.savefig("cm.png")
+# save figures
+cm.savefig("results/cm.png")
+sample_img.savefig("results/sample_img.png")
+sample_img_pred.savefig("results/sample_img_pred.png")
 
 
-# save model
-model.save("model/CustomModel")
